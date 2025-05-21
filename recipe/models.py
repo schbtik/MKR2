964d714159c2fa2ba9520adc58495092060f54e8
Sofiia Scherbatiuk <sfchbtik@gmail.com>
2025-05-21 12:58:45 +0300

    Додала модель рецепт та методи відповідні

--- recipe/models.py
+++ recipe/models.py
@@ -10,3 +10,14 @@ class Category(models.Model):
         return iter([])
 
 
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
